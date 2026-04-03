e4619fbc0d72d18af913cf07b7761d461fb5d55b
LauraMefinya <22556754@stu.mmu.ac.uk>
2026-04-03 12:18:52 +0200

    Add ProfilePage

---
+++ Views/ProfilePage.xaml.cs
@@ -0,0 +1,10 @@
+namespace BillWise.Views
+{
+    public partial class ProfilePage : ContentPage
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
